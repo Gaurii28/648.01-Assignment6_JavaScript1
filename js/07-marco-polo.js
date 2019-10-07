ed402aac953d2911c4368a387998c183068a439c
Gaurii28 <gauripatel2803@gmail.com>
2019-10-06 22:23:29 -0700

    Update 07-marco-polo.js

--- js/07-marco-polo.js
+++ js/07-marco-polo.js
@@ -1,5 +1,6 @@
 for(var i=1;i<=100;i++){
-    if(i%3 == 0 && i%5 == 0){document.write("Marco!Polo! <br> <br>");}
-    else if(i% 3 == 0 ){document.write("Marco! <br> <br>");}
-    else if(i% 5 == 0){document.write("Polo! <br> <br>");} 
+    if(i%3 == 0 && i%5 == 0){document.write("Marco!Polo! <br>");}
+    else if(i% 3 == 0 ){document.write("Marco! <br> ");}
+    else if(i% 5 == 0){document.write("Polo! <br> ");} 
+    else {document.write(i+'</br>');}
 }
